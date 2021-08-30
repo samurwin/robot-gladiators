d9ed81f1ef7587b4c25e188b7d897525a59fa514
smudesign <urwinsamantha@gmail.com>
2021-08-30 17:46:34 -0400

    fight function added

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,19 +1,54 @@
-// Set your player or robot name
+// Player info
 var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-// Developer console
-console.log(playerName);
-console.log("This logs a string, good for leaving yourself a message");
-// this will do math and log 20
-console.log(10 + 10);
-//
-console.log("Our robot's name is " + playerName);
+console.log(playerName, playerAttack, playerHealth);
 
+// Enemy info
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-//Start the fight alert
-function fight() {
-    window.alert("The fight has begun!");
-}
+console.log(enemyName, enemyAttack, enemyHealth);
 
+//Create Fight Function
+var fight = function() {
+    // Alert players that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
 
-// fight();
+    //Subtract the value of 'playerAttack' from the value of 'enemyHealth'
+    enemyHealth = enemyHealth - playerAttack;
+
+    //Log a resulting message to the console so we know it worked
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining. "
+        );
+    
+    //Check enemy Health
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died ");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+
+    //Subtract the value of 'enemyAttack' from the value of 'playerHealth
+    playerHealth = playerHealth - enemyAttack;
+
+    //Log a resulting message to the console so we know it worked
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining. "
+        );
+    //Check Player health
+    if (playerHealth <= 0) {
+       window.alert(playerName + " has died. ");
+    }
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
+
+};
+
+// Execute Function
+fight();
